f8723e2ea342576ee128ed8b3fa44e4bd4cb2eef
ishaan2020 <53574413+ishaan2020@users.noreply.github.com>
2021-01-09 19:04:06 -0500

    random change

--- app/src/main/java/com/example/nwhacks2021/quizbee/DeveloperActivity.java
+++ app/src/main/java/com/example/nwhacks2021/quizbee/DeveloperActivity.java
@@ -1,10 +1,13 @@
 package com.example.nwhacks2021.quizbee;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nwhacks2021.R;
 
 public class DeveloperActivity extends AppCompatActivity {
     Button btnRestart;
--- app/src/main/java/com/example/nwhacks2021/quizbee/ResultActivity.java
+++ app/src/main/java/com/example/nwhacks2021/quizbee/ResultActivity.java
@@ -1,12 +1,15 @@
 package com.example.nwhacks2021.quizbee;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nwhacks2021.R;
 
 import org.w3c.dom.Text;
 
